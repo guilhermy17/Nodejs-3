0a101044705eadfdf1aae8c24a9148e86cfb364d
Guilhermy <133382489+guilhermy17@users.noreply.github.com>
2024-09-02 13:21:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log(`Trabalhando com condicionais.`);
+
+console.log(`Destinos oferecidos:`);
+
+const listaDeDestinos = new Array(
+  `Kyoto`
+  `Shenzhen`
+  `Bali`
+  `Bangkok`
+  `Moscou`
+);
+
+console.log(`listaDeDestinos`);
+
+const idadeComprador = 18;
+const menorAcompanhado
+
+if(idadeComprador < 18) {
+  console.log(`Comprador menor de idade, não podemos vender as passagens.`);
+}
+
+ if(menorAcompanhado == true){
+  console.log(`O menor encontra-se acompanhado, então ele pode realizar a viagem.`);
+}
+else{
+  console.log(`Qual seu destino?`);
+  console.log(listaDeDestinos[1]);
+}
